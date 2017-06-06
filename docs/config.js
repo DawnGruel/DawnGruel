a7b85674009d05e01af7b9e6c66ea8624bff1bf8
Dylan <247328581@qq.com>
2017-06-06 16:53:54 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -17,25 +17,6 @@ var $config = {
         },
         { title: 'ES6', path: '/ES6/ES6'},
         { title: 'webpack', path: '/webpack/'},
-    ],
-    //   plugins: [
-    //   docsearch({
-    //     apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
-    //     indexName: 'H5',
-    //     // docsearch 允许你把抓取的内容按照 tag 分类
-    //     // 详情请联系 algolia 客服，这里你只需要把你想搜索的 tag 填进来就行了
-    //   })
-    // ],
-    // // 这个插件同时需要你的 URL
-    // // 因为 docsearch 是按照你的线上 URL 抓取内容的
-    // url: 'https://docute.js.org'
-     plugins: [
-    docsearch({
-      apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
-        indexName: 'docs',
-        tags:['ES6','basis','webpack'],
-      url: 'https://dylan-wy.github.io/DawnGruel/docs'
-    }),
-  ]
+    ] 
 }
 docute.init($config)
