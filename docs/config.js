dc1c59a5cf97860d76d8675d635332813d5929f3
Dylan <247328581@qq.com>
2017-06-06 16:47:08 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -33,6 +33,7 @@ var $config = {
     docsearch({
       apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
         indexName: 'DawnGruel',
+        tags:['ES6','basis','webpack']
       url: 'https://dylan-wy.github.io'
     }),
   ]
