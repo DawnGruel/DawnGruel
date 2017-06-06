2e353da6bf8f191b7bcb8acda95573d48d95998f
Dylan <247328581@qq.com>
2017-06-06 16:38:48 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -32,6 +32,7 @@ var $config = {
      plugins: [
     docsearch({
       apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
+        indexName: 'docs',
       url: 'https://dylan-wy.github.io/DawnGruel/docs'
     }),
   ]
